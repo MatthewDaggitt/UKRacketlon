aff699cc2953f8de098e6ec3809450d1cbb03f96
Matthew <matthewdaggitt@gmail.com>
2016-06-10 08:04:20 +0100

    Cleaned up the css file somewhat

--- tmpl_racketlon/index.php
+++ tmpl_racketlon/index.php
@@ -3,27 +3,26 @@ defined('_JEXEC') or die;
 ?>
 
 <?php
-/**
- * Template for Joomla! CMS, created with Designer.
- * See readme.txt for more details on how to use the template.
- */
-header('Content-Type: text/html; charset=UTF-8');
+	/**
+	 * Template for Joomla! CMS, created with Designer.
+	 * See readme.txt for more details on how to use the template.
+	 */
+	header('Content-Type: text/html; charset=UTF-8');
 
-$themeDir = dirname(__FILE__);
+	$themeDir = dirname(__FILE__);
+	require_once $themeDir . DIRECTORY_SEPARATOR . 'functions.php';
 
-require_once $themeDir . DIRECTORY_SEPARATOR . 'functions.php';
-// Create alias for $this object reference:
-$document = $this;
-$document->head = "<jdoc:include type=\"head\" />";
-// Shortcut for template base url:
-$templateUrl = $document->baseurl . '/templates/' . (isset($editorDir) ? $editorDir : $document->template);
-$document->templateUrl = $templateUrl;
-Designer::load("Designer_Page");
+	// Create alias for $this object reference:
+	$document = $this;
+	$document->head = "<jdoc:include type=\"head\" />";
+	// Shortcut for template base url:
+	$templateUrl = $document->baseurl . '/templates/' . (isset($editorDir) ? $editorDir : $document->template);
+	$document->templateUrl = $templateUrl;
+	Designer::load("Designer_Page");
 
-// Initialize $view:
-$this->view = new DesignerPage($this);
-echo $this->view->renderTemplate($themeDir);
+	// Initialize $view:
+	$this->view = new DesignerPage($this);
+	echo $this->view->renderTemplate($themeDir);
 ?>
 
-<script type="text/javascript"
-   src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
